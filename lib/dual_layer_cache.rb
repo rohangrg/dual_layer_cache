3fc84e90ff34593b6cd5798deb4af410aa28fe5e
Rohan Garg <rohangarg32767@gmail.com>
2025-02-20 20:46:32 +0400

    minor changes

--- lib/dual_layer_cache.rb
+++ lib/dual_layer_cache.rb
@@ -1,4 +1,10 @@
+# /Users/rohangarg/.rvm/gems/ruby-3.2.2/bundler/gems/dual_layer_cache-62723320652d/lib/dual_layer_cache.rb
+# frozen_string_literal: true
+
+require 'active_support/cache'
 require 'dual_layer_cache/store'
 require 'dual_layer_cache/rebuild_cache_job'
 
-ActiveSupport::Cache::Store.inherited(DualLayerCache::Store)
+module DualLayerCache
+  # No need for explicit inherited call; subclassing is sufficient
+end
--- lib/dual_layer_cache/store.rb
+++ lib/dual_layer_cache/store.rb
@@ -1,28 +1,28 @@
 # lib/dual_layer_cache/store.rb
-require 'active_support/cache'
-
 module DualLayerCache
   class Store < ActiveSupport::Cache::Store
-    @@blocks = {} # Store blocks for background rebuilding
-
-    def self.blocks
-      @@blocks
-    end
+    @@rebuilders = {}
 
     def initialize(base_store_options = {})
       @base_store = ActiveSupport::Cache::RedisCacheStore.new(base_store_options)
     end
 
-    def read(key, options = nil)
+    def fetch(key, options = nil, &block)
+      @@rebuilders[key] = options[:rebuilder] if options[:rebuilder]
+
       r1_key = "r1:#{key}"
       value = @base_store.read(r1_key, options)
       return value if value
 
       r2_key = "r2:#{key}"
       value = @base_store.read(r2_key, options)
-      if value && @@blocks[key]
-        RebuildCacheJob.perform_later(key, @@blocks[key])
+      if value
+        RebuildCacheJob.perform_later(key, @@rebuilders[key]) if @@rebuilders[key]
+        return value
       end
+
+      value = block.call
+      write(key, value, options)
       value
     end
 
@@ -33,28 +33,13 @@ module DualLayerCache
       @base_store.write(r2_key, value, options)
     end
 
-    def fetch(key, options = nil, &block)
-      @@blocks[key] = block if block_given?
-
+    def read(key, options = nil)
       r1_key = "r1:#{key}"
       value = @base_store.read(r1_key, options)
       return value if value
 
       r2_key = "r2:#{key}"
-      value = @base_store.read(r2_key, options)
-      if value
-        RebuildCacheJob.perform_later(key, @@blocks[key]) if @@blocks[key]
-        return value
-      end
-
-      # Cold start: compute synchronously and store
-      if block_given?
-        value = block.call
-        write(key, value, options)
-        return value
-      else
-        raise "No block provided for cold start on key: #{key}"
-      end
+      @base_store.read(r2_key, options)
     end
 
     def delete(key, options = nil)
